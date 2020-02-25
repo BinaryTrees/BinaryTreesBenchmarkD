c39e6fd9a315ce2e94085d4842a45268bebdc4c4
Akira13641 <operator97@gmail.com>
2020-02-25 15:43:40 -0500

    Better use of static if

--- source/pooledmm.d
+++ source/pooledmm.d
@@ -24,34 +24,60 @@ public:
   ~this() {
     clear();
   }
-
-  nothrow @nogc void clear() {
-    if (items.length > 0) {
-      for (size_t i = 0; i < items.length; ++i) {
-        static if(hasElaborateDestructor!(T)) {
+  
+  static if(hasElaborateDestructor!(T)) {
+    void clear() {
+      if (items.length > 0) {
+        for (size_t i = 0; i < items.length; ++i) {
           destroy(*(cast(T*) items[i]));
+          free(items[i]);
         }
-        free(items[i]);
+        // Dvector's `free` member function is what other libraries more often call `clear`, BTW.
+        items.free();
       }
-      // Dvector's `free` member function is what other libraries more often call `clear`, BTW.
-      items.free();
+      curSize = T.sizeof * 4;
+      curItem = null;
+      endItem = null;
     }
-    curSize = T.sizeof * 4;
-    curItem = null;
-    endItem = null;
-  }
-
-  nothrow @nogc T* newItem() {
-    if (curItem == endItem) {
-      curSize += curSize;
-      curItem = malloc(curSize);
-      items.pushBack(curItem);
-      endItem = curItem;
-      endItem += curSize;
+    
+    T* newItem() {
+      if (curItem == endItem) {
+        curSize += curSize;
+        curItem = malloc(curSize);
+        items.pushBack(curItem);
+        endItem = curItem;
+        endItem += curSize;
+      }
+      T* result = cast(T*) curItem;
+      curItem += T.sizeof;
+      *result = T.init;
+      return result;
     }
-    T* result = cast(T*) curItem;
-    curItem += T.sizeof;
-    *result = T.init;
-    return result;
+  } else {
+    nothrow @nogc void clear() {
+      if (items.length > 0) {
+        for (size_t i = 0; i < items.length; ++i)
+          free(items[i]);
+        // Dvector's `free` member function is what other libraries more often call `clear`, BTW.
+        items.free();
+      }
+      curSize = T.sizeof * 4;
+      curItem = null;
+      endItem = null;
+    }
+    
+    nothrow @nogc T* newItem() {
+      if (curItem == endItem) {
+        curSize += curSize;
+        curItem = malloc(curSize);
+        items.pushBack(curItem);
+        endItem = curItem;
+        endItem += curSize;
+      }
+      T* result = cast(T*) curItem;
+      curItem += T.sizeof;
+      memset(result, 0, T.sizeof);
+      return result;
+    }
   }
 }
